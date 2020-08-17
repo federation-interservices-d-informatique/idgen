0c0d0d50ce7a2b0d45a331abd0dc1f1a8bda8bc8
Woomy4680-exe <matzar1808@gmail.com>
2020-08-17 17:39:56 +0200

    Check server name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let servers = vec!("MIM", "TH", "LPT", "TAB");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2  {
+        println!("Pas assez d'arguments!"); 
+        std::process::exit(1); 
+    }
+    if ! servers.iter().any(|v| v.to_owned() == &args[1].to_uppercase()) {
+        println!("Nom de serveur invalide!");
+        std::process::exit(1);
+    }
 }
