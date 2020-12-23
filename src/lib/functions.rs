839db3ca109796acad70af2b77597de28a8d671a
Rheydskey <matgag02@gmail.com>
2020-12-23 22:14:09 +0100

    :arrow_up: Upgrade rand (0.7.3 >> 0.8.0).

--- src/lib/functions.rs
+++ src/lib/functions.rs
@@ -1,6 +1,6 @@
 use rand::prelude::*;
 pub fn gen_random() -> i64 {
-    return rand::thread_rng().gen_range(1000000000, 9999999999);
+    return rand::thread_rng().gen_range(1000000000..9999999999);
 }
 pub fn checknum(num: String) -> String {
     match num.len() {
