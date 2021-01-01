38e042411e78affdd13165340212a4955ae5f3f7
Woomy4680-exe <matzar1808@gmail.com>
2021-01-01 17:16:10 +0100

    ⚡️ Change how checknum function works

--- src/lib/functions.rs
+++ src/lib/functions.rs
@@ -3,26 +3,12 @@ pub fn gen_random() -> i64 {
     return rand::thread_rng().gen_range(1000000000..9999999999);
 }
 pub fn checknum(num: String) -> String {
-    match num.len() {
-        1 => {
-            return format!("0000{}", num);
-        }
-        2 => {
-            return format!("000{}", num);
-        }
-        3 => {
-            return format!("00{}", num);
-        }
-        4 => {
-            return format!("0{}", num);
-        }
-        5 => {
-            return format!("{}", num);
-        }
-        _ => {
-            return "00000".to_owned();
-        }
+    let mut n: String;
+    n = num.clone();
+    while num.len() >= 10 {
+        n = format!("0{}", n.clone());
     }
+    return num.to_string();
 }
 pub fn usage() {
     let argsvec: Vec<String> = std::env::args().collect();
